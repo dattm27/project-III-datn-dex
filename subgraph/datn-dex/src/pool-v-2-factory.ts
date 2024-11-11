d5fed30685cc3ac9c9ad1157bf6d77e60e8fb3da
dattm27 <dattran2003.ttn@gmail.com>
2024-11-12 01:48:43 +0700

    subgrapH: indexing pool done

--- subgraph/datn-dex/src/pool-v-2-factory.ts
+++ subgraph/datn-dex/src/pool-v-2-factory.ts
@@ -2,7 +2,9 @@ import {
   Initialized as InitializedEvent,
   PoolCreated as PoolCreatedEvent
 } from "../generated/PoolV2Factory/PoolV2Factory"
-import { Initialized, PoolCreated } from "../generated/schema"
+import { Initialized, Pool, Token } from "../generated/schema"
+import {ERC20} from "../generated/PoolV2Factory/ERC20"
+import { Address } from "@graphprotocol/graph-ts"
 
 export function handleInitialized(event: InitializedEvent): void {
   let entity = new Initialized(
@@ -18,11 +20,18 @@ export function handleInitialized(event: InitializedEvent): void {
 }
 
 export function handlePoolCreated(event: PoolCreatedEvent): void {
-  let entity = new PoolCreated(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
+  let entity = new Pool(
+    event.params.pair.toHex()
   )
-  entity.token0 = event.params.token0
-  entity.token1 = event.params.token1
+
+
+  //contract instance 
+  let token0 = getTokenDetails(event.params.token0)
+  let token1 = getTokenDetails(event.params.token1)
+
+  entity.token0 = token0.id
+  entity.token1 = token1.id
+
   entity.pair = event.params.pair
   entity.param3 = event.params.param3
 
@@ -31,4 +40,23 @@ export function handlePoolCreated(event: PoolCreatedEvent): void {
   entity.transactionHash = event.transaction.hash
 
   entity.save()
+}
+
+//check if token exists and create if needed
+function getTokenDetails(tokenAddress: Address): Token {
+  let token = Token.load(tokenAddress.toHex())
+  if (token == null) {
+    token = new Token(tokenAddress.toHex())
+    let tokenContract = ERC20.bind(tokenAddress)
+
+    let tokenName = tokenContract.try_name()
+    let tokenSymbol = tokenContract.try_symbol()
+    let tokenDecimals = tokenContract.try_decimals()
+
+    token.name = !tokenName.reverted ? tokenName.value : "Unknown Token"
+    token.symbol = !tokenSymbol.reverted ? tokenSymbol.value : "UNKNOWN"
+    token.decimals = !tokenDecimals.reverted ? tokenDecimals.value : 18
+    token.save()
+  }
+  return token as Token
 }
